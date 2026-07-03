bf8f9b412bd3737ddf02b682f63d522194660650
Glen Bangkila <hadeinvane@gmail.com>
2026-07-03 18:08:18 +0800

    chore: bump version to 0.1.2

--- Project.swift
+++ Project.swift
@@ -42,7 +42,7 @@ let project = Project(
                 "CFBundleName": "Zetty",
                 "CFBundleDisplayName": "Zetty",
                 "CFBundleIconFile": "AppIcon",
-                "CFBundleShortVersionString": "0.1.1",
+                "CFBundleShortVersionString": "0.1.2",
             ]),
             sources: ["App/Sources/App/**"],
             resources: ["App/Resources/**/*.svg", "App/Resources/*.icns",
